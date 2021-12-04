302239bd17cd43759c277c62497a32f9f2c83c37
Charles Pisciotta <cpisciottadeveloping@gmail.com>
2021-12-04 18:14:53 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "FirebaseSwift",
     dependencies: [
-        .Package(url: "https://github.com/gtchance/Just.git", majorVersion: 0, minor: 6)
+        .Package(url: "https://github.com/cpisciotta/Just.git", , .branch("master"))
     ]
 
 )
