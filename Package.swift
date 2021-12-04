5373377c168ed4fb4f6609a863e476ef64f38a71
Charles Pisciotta <cpisciottadeveloping@gmail.com>
2021-12-04 18:17:52 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,13 @@ let package = Package(
     name: "FirebaseSwift",
     dependencies: [
         .package(url: "https://github.com/cpisciotta/Just.git", .branch("master"))
+    ],
+    targets: [
+        .target(
+            name: "FirebaseSwift",
+            dependencies: [
+                .product(name: "Just", package: "Just"),
+            ]
+        )
     ]
-
 )
